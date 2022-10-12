254ff6023f20bb4c31d9793a5c1ba6948ac067ec
danielsfa92 <109553743+danielsfa92@users.noreply.github.com>
2022-10-12 10:18:38 -0300

    Componentes principal e auxiliar

---
+++ src/Auxiliar.js
@@ -0,0 +1,13 @@
+function Auxiliar() {
+
+  return ( <
+    div >
+    <
+    p > Componente auxiliar < /p> <
+    /div>
+
+  )
+
+}
+
+export default Auxiliar
---
+++ src/Principal.js
@@ -0,0 +1,16 @@
+import Auxiliar from './Auxiliar'
+
+function Principal() {
+
+  return ( <
+    div >
+    <
+    h1 > Componente principal < /h1> <
+    Auxiliar / >
+    <
+    /div>
+  )
+
+}
+
+export default Principal
